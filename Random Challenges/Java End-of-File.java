e60ee34acd6e9380b46c1a26b0bfca9aa86090f5
Nouf AlSalem <61601877+noufalsalem@users.noreply.github.com>
2022-07-19 06:01:34 +0300

    Create Java End-of-File.java

---
+++ Random Challenges/Java End-of-File.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        
+        int n = 0;
+        while (input.hasNext()) {
+            n++;
+            System.out.println(n + " " + input.nextLine()); 
+        }
+        
+        
+    }
+}

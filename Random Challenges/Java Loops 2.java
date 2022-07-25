c5dcc75a1dcc6e507b29a7120b82b16b1f08e084
Nouf AlSalem <61601877+noufalsalem@users.noreply.github.com>
2022-07-25 07:29:12 +0300

    Update Java Loops 2.java

--- Random Challenges/Java Loops 2.java
+++ Random Challenges/Java Loops 2.java
@@ -3,18 +3,20 @@ import java.util.*;
 class Solution{
     public static void main(String []argh){
         Scanner scanner = new Scanner(System.in);
-        int t=scanner.nextInt();
-        for(int i=0;i<t;i++){
-            int a = scanner.nextInt();
-            int b = scanner.nextInt();
-            int n = scanner.nextInt();            
-            int sum = a;
-            for (int j = 0; j<n; j++) {                
-                int nextEle = (int)Math.pow(2, j)*b;
-                sum = sum + nextEle;
-                System.out.printf("%s ",sum);                                
+        int query =scanner.nextInt(); //3
+        for(int i=0; i<query; i++){
+        
+            int a = scanner.nextInt(); //1
+            int b = scanner.nextInt(); //2
+            int n = scanner.nextInt(); //3      
+            int sum = a; //1 
+            
+            for (int j = 0; j<n; j++) { // j < 3          
+                int nextEl = (int)Math.pow(2, j)*b; // nextEl = (2^j) x b = 2 x 2
+                sum = sum + nextEl; // 1 + 4
+                System.out.printf("%s ",sum); //5                            
             }
-            if (i < t-1) {
+            if (i < query-1) { 
                 System.out.print("\n");
             }
             
